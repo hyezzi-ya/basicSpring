bc82eb8d1bbba328edb31cd0b3b5fd467c6b4975
hyejin <hyezzi-ya@naver.com>
2023-11-15 11:34:06 +0900

    @Component 애노테이션
    이제 각 클래스가 컴포넌트 스캔의 대상이 되도록 @Component 애노테이션을 붙여줌

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,17 +3,21 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    //테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+//    //테스트 용도
+//    public MemberRepository getMemberRepository() {
+//        return memberRepository;
+//    }
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
