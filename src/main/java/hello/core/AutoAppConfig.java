2266f7388d7143d44695b6f0f20e3391c95b0184
hyejin <hyezzi-ya@naver.com>
2023-11-15 12:37:47 +0900

    수동 주입 주석처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -21,8 +21,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
         */
 )
 public class AutoAppConfig {
-        @Bean(name = "memoryMemberRepository")
-        public MemberRepository memberRepository() {
-                return new MemoryMemberRepository();
-        }
+//        @Bean(name = "memoryMemberRepository")
+//        public MemberRepository memberRepository() {
+//                return new MemoryMemberRepository();
+//        }
 }
